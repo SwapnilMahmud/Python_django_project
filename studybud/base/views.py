b74ab14ebbce8beec07e01812435f70460e35751
Swapnil Mahmud <mahmudaiub51@gmail.com>
2022-07-31 00:07:01 +0600

    base urls added

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse('Home page')
+
+def about(request):
+    return HttpResponse("about page")
+ 
+def contact(request):
+    return HttpResponse("contact")
--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -14,20 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
+# from django.http import HttpResponse
 
-# def home(request):
-#     return HttpResponse('Home page')
 
-# def about(request):
-#     return HttpResponse("about page")
- 
-# def contact(request):
-#     return HttpResponse("contact")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('base.urls'))
     # path('', home),
     # path('about/',about),
     # path('contact/',contact),
