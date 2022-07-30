dc944fab2053561610b7cf5d6c4c949cbaf854ef
Swapnil Mahmud <mahmudaiub51@gmail.com>
2022-07-31 01:29:17 +0600

    new views

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request,'home.html')
 
 def about(request):
-    return HttpResponse("about page")
+    return render(request,'about.html')
  
 def contact(request):
-    return HttpResponse("contact")
+    return render(request,'contact.html')
