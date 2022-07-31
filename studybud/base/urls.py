7d0f57daaf8e8d46b3702ec006df437e268ba89d
Swapnil Mahmud <mahmudaiub51@gmail.com>
2022-08-01 01:23:45 +0600

    new template added

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns=[
     path('', views.home, name="home"),
     path('about/',views.about, name="about"),
-    path('contact/',views.contact,name="contact")
+    path('contact/',views.contact,name="contact"),
+    path('room/',views.room,name="room"),
+    path('main/',views.main,name="main")
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+rooms = [
+    {'id':1, 'name':'Lets learn python'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developers'},
+]
+
 def home(request):
-    return render(request,'home.html')
+    return render(request,'home.html',{'rooms':rooms})
 
 def about(request):
     return render(request,'about.html')
  
 def contact(request):
     return render(request,'contact.html')
+
+def room(request):
+    return render(request,'room.html')
+
+def main(request):
+    return render(request,'main.html')
